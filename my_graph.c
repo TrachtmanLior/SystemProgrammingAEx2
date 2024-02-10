aed78eea88d886ae17112b9b61812ad56cd42cd2
SamuraiPolix <sam.lazareanu@gmail.com>
2024-02-10 15:40:56 +0200

    run floyd's algo only when necessary (once for every new matrix)

--- my_graph.c
+++ my_graph.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "my_mat.h"
+#include <stdbool.h>
 
 int main(){
 
@@ -7,21 +8,30 @@ int main(){
     char operator;
 
     int matrix[MATRIX_SIZE][MATRIX_SIZE];
+    bool didAlgoRun = false;        // Tell us if the Floyd's algo already ran for the current matrix - so we don't run it again
 
     scanf("%c", &operator);
 
+    // Assuming the user enters values to matrix (A) before trying B or C
     switch(operator){
         case 'A':
             getMatrix(matrix, MATRIX_SIZE);
+            didAlgoRun = false;
             break;
         case 'B':
             scanf("%d%d", &i, &j);
-            floydWarshallAlgorithm(matrix, MATRIX_SIZE);
+            if (!didAlgoRun) {
+                floydWarshallAlgorithm(matrix, MATRIX_SIZE);
+                didAlgoRun = true;
+            }
             printf("%s\n", existPath(matrix, i, j, MATRIX_SIZE) ? "True":"False");
             break;
         case 'C':
             scanf("%d%d", &i, &j);
-            floydWarshallAlgorithm(matrix, MATRIX_SIZE);
+            if (!didAlgoRun) {
+                floydWarshallAlgorithm(matrix, MATRIX_SIZE);
+                didAlgoRun = true;
+            }            
             printf("%d\n", shortestPath(matrix, i, j, MATRIX_SIZE));
             break;
         case EOF:

0a8d991fbfeb0efe130f4fd05a4e7e6f1e51383c
SamuraiPolix <sam.lazareanu@gmail.com>
2024-02-10 15:34:38 +0200

    change matrix to static memory

--- my_graph.c
+++ my_graph.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "my_mat.h"
 
 int main(){
@@ -7,37 +6,28 @@ int main(){
     int i,j;
     char operator;
 
-    // Allocate memory
-    int **matrix = (int**) malloc(SIZE * sizeof(int*));
-
-    for (int i = 0; i < SIZE; ++i)
-        matrix[i] = (int*) malloc(SIZE * sizeof(int));
+    int matrix[MATRIX_SIZE][MATRIX_SIZE];
 
     scanf("%c", &operator);
 
     switch(operator){
         case 'A':
-            getMatrix(matrix, SIZE);
+            getMatrix(matrix, MATRIX_SIZE);
             break;
-
         case 'B':
             scanf("%d%d", &i, &j);
-            printf("%s\n", existPath(matrix, i, j, SIZE) ? "True":"False");
+            floydWarshallAlgorithm(matrix, MATRIX_SIZE);
+            printf("%s\n", existPath(matrix, i, j, MATRIX_SIZE) ? "True":"False");
             break;
         case 'C':
             scanf("%d%d", &i, &j);
-            printf("%d\n", shortestPath(matrix, i, j, SIZE));
+            floydWarshallAlgorithm(matrix, MATRIX_SIZE);
+            printf("%d\n", shortestPath(matrix, i, j, MATRIX_SIZE));
             break;
+        case EOF:
         case 'D':
             break;
-
     }
-
-    // Free memory to end program
-    for (int i = 0; i < SIZE; ++i)
-        free(matrix[i]);
-
-    free(matrix);
 
     return 0;
 }
--- my_mat.h
+++ my_mat.h
@@ -1,16 +1,19 @@
 #ifndef MY_MAT_H
 #define MY_MAT_H
-#define SIZE 10
+#define MATRIX_SIZE 10
 #define INFINITY INT_MAX
 
 //case A get a matrix[i][j]
-void getMatrix(int**, int);
+void getMatrix(int[][], int);
 
 //case B check for a path from i to j
 int existPath(int**, int, int, int);
 
 //case C find shortestpath from i to j
 int shortestPath(int**, int, int, int);
+
+//Algo to set shortest path in each cell
+void floydWarshallAlgorithm(int[][], int size);
 
 
 #endif

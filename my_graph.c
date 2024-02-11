7f240b1ffe1ff0bbab2839e5324be216076584d5
Trachtman Lior <155372457+TrachtmanLior@users.noreply.github.com>
2024-02-11 16:24:21 +0000

    Update my_graph.c

--- my_graph.c
+++ my_graph.c
@@ -9,12 +9,13 @@ int main(){
     int matrix[MATRIX_SIZE][MATRIX_SIZE];
     bool didAlgoRun = false;        // Tell us if the Floyd's algo already ran for the current matrix - so we don't run it again
     bool keepGettingInput = true;       // false when 'D' or EOF is inputed
+    bool dropLine = false;       // used to drop line only before B or C new inputs
     
 
     // Assuming the user enters values to matrix (A) before trying B or C
     while (keepGettingInput){
         scanf("%c", &operator);
-        if (operator != ' ' && operator != 'D' && operator != EOF && operator != 'A'){
+        if (dropLine && operator != ' ' && operator != 'D' && operator != EOF && operator != 'A'){
             printf("\n");
         }
         switch(operator){
@@ -29,6 +30,7 @@ int main(){
                 didAlgoRun = true;
             }
             printf("%s", existPath(matrix, i, j, MATRIX_SIZE) ? "True":"False");
+            dropLine = true;
             break;
         case 'C':
             scanf("%d%d", &i, &j);
@@ -37,6 +39,7 @@ int main(){
                 didAlgoRun = true;
             }            
             printf("%d", shortestPath(matrix, i, j, MATRIX_SIZE));
+            dropLine = true;
             break;
         case EOF:
         case 'D':
